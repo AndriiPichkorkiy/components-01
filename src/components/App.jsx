73c5c9c7e83a2ade8aacf31825096826d3af3fd7
Andrey <93935023+desertptu4ka@users.noreply.github.com>
2022-07-24 14:48:28 +0300

    create form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    isLogedIn: false,
+  };
+
+  isLogedInToggle = event => {
+    event.preventDefault();
+    // this.setState(preState => ({
+    //   isLogedIn: !preState.isLogedIn,
+    // }))
+
+    this.setState(preState => {
+      console.log(event);
+      return {
+        isLogedIn: !preState.isLogedIn,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.isLogedInToggle}>
+        <label htmlFor="name">Name: </label>
+        <input id="name" type="text" name="name" placeholder="write in name" />
+
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import Picachu from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <Picachu />
+  // </React.StrictMode>
+
+  <Picachu />
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// import App from './components/App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
